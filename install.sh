04cf695d3a049cb03ae1cd0b53fdeca77fd45d51
Erik Berkun-Drevnig <erik.b22@gmail.com>
2015-04-30 20:40:12 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# variables
+dir = ~/dotfiles
+olddir = ~/dotfiles_old
+files = "bash_profile"
+
+# create dotfiles_old in homedir
+mkdir $olddir
+
+# change to the dotfiles directory
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    mv ~/.$file ~/dotfiles_old/
+    ln -s $dir/$file ~/.$file
+done
